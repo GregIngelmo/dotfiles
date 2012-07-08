c093e6dc04dd4755968cb471664d9f2332b26974
Greg Ingelmo <git@ingelmo.org>
2012-07-08 16:19:16 -0400

    Updated setup script to look for symlinks instead of files (-L intead of -F), updated comments, added config using global gitignore file

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # Symlink dot files into the home directory
 #
 
-pathsToSymlink="zshrc vimrc gvimrc gitignore"
+pathsToSymlink="zshrc vimrc gvimrc gitignore vim"
 
 keyColor='[38;5;117m' # light blue [0m 
 valColor='[01;33m'    # yellow     [0m 
@@ -12,9 +12,9 @@ noColor='[0m'
 
 for fileName in $pathsToSymlink
 do
-    # if the file doesn't exist in the users home folder then 
-    # symlink it as a hidden file from this directory
-    if [ ! -f "$HOME/.$fileName" ]
+    # If a symlink (-L) doesn't exist in the users home folder then 
+    # symlink it a from the current directory
+    if [ ! -L "$HOME/.$fileName" ]
     then
         echo "Linking ${keyColor}$PWD/$fileName${noColor} -> ${valColor}$HOME/.$fileName${noColor}"
         
@@ -26,4 +26,4 @@ done
 # Configure global git settings for colors & gitignore
 #
 
-# git config --global core.excludesfile '~/.gitignore'
+git config --global core.excludesfile '~/.gitignore'
