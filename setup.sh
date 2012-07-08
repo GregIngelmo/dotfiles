5549d07f5b7a535df38d67792c158808774e3bfa
Greg Ingelmo <git@ingelmo.org>
2012-07-08 16:04:44 -0400

    Rewrite setup script, it now iterates over an array of paths and symlinks each path in the array. Booya bitch

--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
-# Run from inside dotfiles directory
+#
+# Symlink dot files into the home directory
+#
 
-if [ ! -f "$HOME/.zshrc" ]; then
-    echo "Symlinked ${PWD}/zshrc to $HOME/.zshrc"
-    ln -s ${PWD}/zshrc $HOME/.zshrc
-fi
+pathsToSymlink="zshrc vimrc gvimrc gitignore"
 
+keyColor='[38;5;117m' # light blue [0m 
+valColor='[01;33m'    # yellow     [0m 
+noColor='[0m'
+
+for fileName in $pathsToSymlink
+do
+    # if the file doesn't exist in the users home folder then 
+    # symlink it as a hidden file from this directory
+    if [ ! -f "$HOME/.$fileName" ]
+    then
+        echo "Linking ${keyColor}$PWD/$fileName${noColor} -> ${valColor}$HOME/.$fileName${noColor}"
+        
+        ln -s $PWD/$fileName $HOME/.$fileName
+    fi
+done
+
+# 
+# Configure global git settings for colors & gitignore
+#
+
+# git config --global core.excludesfile '~/.gitignore'
