03f641f190fd71d068f393fcaa83f483ee45101f
Greg Ingelmo <git@ingelmo.org>
2013-03-28 13:42:55 -0400

    Fixed iPython readline keyboard shortcuts

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -205,7 +205,7 @@ c.TerminalInteractiveShell.editor = 'vim'
 # 
 # c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete', '"\\C-l": clear-screen', 'set show-all-if-ambiguous on', '"\\C-o": tab-insert', '"\\C-r": reverse-search-history', '"\\C-s": forward-search-history', '"\\C-p": history-search-backward', '"\\C-n": history-search-forward', '"\\e[A": history-search-backward', '"\\e[B": history-search-forward', '"\\C-k": kill-line', '"\\C-u": unix-line-discard']
 
-c.TerminalInteractiveShell.readline_parse_and_bind = ['"\\e[1;5D": backward-word', '"\\e[1;5C": forward-word']
+c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete', '"\\C-l": clear-screen', 'set show-all-if-ambiguous on', '"\\C-o": tab-insert', '"\\C-r": reverse-search-history', '"\\C-s": forward-search-history', '"\\C-p": history-search-backward', '"\\C-n": history-search-forward', '"\\e[A": history-search-backward', '"\\e[B": history-search-forward', '"\\C-k": kill-line', '"\\C-u": unix-line-discard', '"\\e[1;5D": backward-word', '"\\e[1;5C": forward-word', '"\\e[d": delete-word', '"\\e[z": backward-delete-word']
 
 # The part of the banner to be printed after the profile
 # c.TerminalInteractiveShell.banner2 = ''
